a4a060bfe91835bcf1be8a0f1f26c7657aad9c08
ajey108 <ajayxa1@gmail.com>
2024-05-10 17:39:54 +0530

    adding add new  pins

---
+++ routes/index.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var router = express.Router();
+const userModel = require("./users");
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const upload = require('./multer');
+
+passport.use(new localStrategy(userModel.authenticate()));
+
+
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index',{nav:false});
+});
+
+router.get('/register', function (req, res, next) {
+  res.render('register',{nav:false});
+});
+router.get('/profile', isLoggedIn, async function (req, res, next) {
+  const user = await userModel.findOne({username:req.session.passport.user});
+  res.render('profile',{user,nav:true});
+});
+
+
+router.get('/add', isLoggedIn, async function (req, res, next) {
+  const user = await userModel.findOne({username:req.session.passport.user});
+  res.render('add',{user,nav:true});
+});
+
+
+router.post('/fileupload', isLoggedIn, upload.single("image"), async function (req, res, next) {
+ const user = await userModel.findOne({username:req.session.passport.user});
+ user.profileImage = req.file.filename;
+ await user.save();
+ res.redirect("/profile");
+});
+
+router.post('/register', function (req, res, next) {
+  const data = new userModel({
+    username: req.body.username,
+    email: req.body.email,
+    contact: req.body.contact
+
+  })
+
+  userModel.register(data, req.body.password).then(function () {
+    passport.authenticate("local")(req, res, function () {
+      res.redirect('/profile')
+    })
+  })
+});
+
+
+router.post("/login", passport.authenticate("local", {
+  failureRedirect: "/",
+  successRedirect: '/profile'
+}), function (req, res, next) {
+  console.log("uer loged in:",req.user);
+
+});
+
+router.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+})
+
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect("/");
+}
+
+module.exports = router;
+
+
+
---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/pin');
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String, 
+  contact: Number,
+  boards: {
+    type: Array,
+    default: []
+  }
+});
+
+// Plugin passport-local-mongoose to enable username and password authentication
+userSchema.plugin(plm);
+
+module.exports = mongoose.model('User', userSchema); 
